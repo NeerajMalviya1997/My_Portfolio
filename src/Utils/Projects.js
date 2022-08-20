abcf130cadd7733b631138ece9ed05d476d30248
NeerajMalviya1997 <neermalviya1997@gmail.com>
2022-08-20 11:22:10 +0530

    portfolio

---
+++ src/Utils/Projects.js
@@ -0,0 +1,25 @@
+
+export const projects = [
+  {
+    img: "https://static.c.realme.com/IN/thread/1362492550103035904.jpg",
+    title: "Apollo-Clone",
+    des: "Sign-up & Order Within 24 Hours to Get ₹100 Cashback on All Orders Above ₹750. Now Get Express Delivery of Medicines, Healthcare & Baby Products in just 2 HOURS. 24/7 Access Telemedicine. 80K+ Products Available. Trusted Online Pharmacy.",
+    demo: "",
+    live: " https://spiffy-kelpie-76cdb6.netlify.app",
+    gitHub: "https://github.com/NeerajMalviya1997/Apollo-clone",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    img: "https://img.freepik.com/premium-photo/colorful-dramatic-sky-with-cloud-sunset-sky-with-sun-background_33736-1538.jpg?size=626&ext=jpg&ga=GA1.2.1270796124.1660924361",
+    title: "WeatherApp-Clone",
+    des: "This weather app is one of best free weather apps with full features: Local weather, weather map (weather map service) and weather widgets.",
+    demo: "",
+    live: "https://bespoke-melba-54b3ee.netlify.app",
+    gitHub: "https://github.com/NeerajMalviya1997/Weather_app",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+     
+    ],
+  }];
